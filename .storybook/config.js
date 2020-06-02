6a73e3d90844b25fe0e508eb58ec564941339ec3
Anjal-Gupta <anjaligupta091205@gmail.com>
2020-06-02 13:07:51 +0530

    Adding storybook configs

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure,addParameters   } from '@storybook/react';
+import { themes } from '@storybook/theming'
+addParameters({
+    options: {
+      theme: themes.light
+    }
+  })
+  
+  function loadStories() {
+    require('../src/stories');
+  }
+  
+  configure(loadStories, module);
